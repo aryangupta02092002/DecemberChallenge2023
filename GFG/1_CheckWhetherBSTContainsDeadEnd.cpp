f226ddbb9274273959d0b5f87539303dfc3121b1
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-12-01 12:02:11 +0530

    Dec 1 - BST | Inorder

---
+++ GFG/1_CheckWhetherBSTContainsDeadEnd.cpp
@@ -0,0 +1,142 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+// } Driver Code Ends
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+class Solution{
+  public:
+    set<int>s;
+    vector<int>leaf;
+    void inor(Node *root)
+    {
+
+        if(root!=NULL){
+            if(root->left==NULL and root->right==NULL){
+                leaf.push_back(root->data);
+            }
+            
+            inor(root->left);
+            s.insert(root->data);
+            inor(root->right);
+        }
+    }
+    
+    bool isDeadEnd(Node *root)
+    {
+        inor(root);
+        for(auto i:leaf){
+            if(i==1){
+                if(s.find(i+1)!=s.end())
+                    return true;
+            }
+
+            if(s.find(i-1)!=s.end() and s.find(i+1)!=s.end()){
+                return true;
+            }
+        }
+        return false;
+
+    }
+};
+
+//{ Driver Code Starts.
+// bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+     Solution ob;
+     cout<<ob.isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends
