a93488668f4fe3ee7836546de7da0c1c57b6ccf7
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-12-25 11:50:20 +0530

    Dec 25 - Strings | DP

---
+++ Leetcode/25_91_DecodeWays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int sz=s.size();
+        vector<int> dp(sz+1, 0);
+        dp[sz] = 1;
+        for(int i=sz-1; i>=0; i--){
+            if(s[i] != '0'){
+                dp[i] += dp[i+1];
+            }
+            if(i+1 < s.size() && (s[i]=='1' || s[i] == '2' && s[i+1] <= '6'))
+                dp[i] += dp[i+2];
+        }
+        return dp[0];
+    }
+};
